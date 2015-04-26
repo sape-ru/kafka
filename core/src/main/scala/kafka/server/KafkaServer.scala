96995e6db00838d6fb90ef7408b3e416feadbb5c
Gwen Shapira <cshapi@gmail.com>
2015-04-26 13:09:00 -0700

    KAFKA-2114 Unable to change min.insync.replicas default; reviewed by Neha

--- core/src/main/scala/kafka/server/KafkaServer.scala
+++ core/src/main/scala/kafka/server/KafkaServer.scala
@@ -320,7 +320,8 @@ class KafkaServer(val config: KafkaConfig, time: Time = SystemTime) extends Logg
                                      deleteRetentionMs = config.logCleanerDeleteRetentionMs,
                                      fileDeleteDelayMs = config.logDeleteDelayMs,
                                      minCleanableRatio = config.logCleanerMinCleanRatio,
-                                     compact = config.logCleanupPolicy.trim.toLowerCase == "compact")
+                                     compact = config.logCleanupPolicy.trim.toLowerCase == "compact",
+                                     minInSyncReplicas = config.minInSyncReplicas)
     val defaultProps = defaultLogConfig.toProps
     val configs = AdminUtils.fetchAllTopicConfigs(zkClient).mapValues(LogConfig.fromProps(defaultProps, _))
     // read the log configurations from zookeeper
