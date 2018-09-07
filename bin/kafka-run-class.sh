f8d0b39228016bf2df4639b604053871710525d8
Andras Beni <andrasbeni@cloudera.com>
2018-09-07 11:30:37 +0200

    [CLOUDERA-BUILD] [CDH-72651] Remove final classpath from tools' output
    Change-Id: I1f6d5d31600ae2c576bdb8e8de8e981a0e2f3f13

--- bin/kafka-run-class.sh
+++ bin/kafka-run-class.sh
@@ -318,8 +318,6 @@ CLASSPATH=${CLASSPATH#:}
 (( CYGWIN )) && CLASSPATH=$(cygpath --path --mixed "${CLASSPATH}")
 
 
-echo "Final CLASSPATH is $CLASSPATH"
-
 # Launch mode
 if [ "x$DAEMON_MODE" = "xtrue" ]; then
   nohup $JAVA $KAFKA_HEAP_OPTS $KAFKA_JVM_PERFORMANCE_OPTS $KAFKA_GC_LOG_OPTS $KAFKA_JMX_OPTS $KAFKA_LOG4J_OPTS -cp $CLASSPATH $KAFKA_OPTS "$@" > "$CONSOLE_OUTPUT_FILE" 2>&1 < /dev/null &
